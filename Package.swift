7c70ccdfba0325a960deca37f71c20771a9b3c44
Alejandro Ramirez Aldariz <627538+janodev@users.noreply.github.com>
2022-03-25 18:48:38 +0100

    no message

--- Package.swift
+++ Package.swift
@@ -14,12 +14,12 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Realm",
-            url: "https://dl.dropboxusercontent.com/s/zetjlp8ynolifkm/Realm.xcframework.zip?dl=0",
+            url: "https://dl.dropboxusercontent.com/s/zetjlp8ynolifkm/Realm.xcframework-10.24.2.zip?dl=0",
             checksum: "fac5fcf18b21e772098cfaff02324b7f788e3918db906f30398ea1d4708f0053"
         ),
         .binaryTarget(
             name: "RealmSwift",
-            url: "https://dl.dropboxusercontent.com/s/3kzrc04umi0ty40/RealmSwift.xcframework.zip?dl=0",
+            url: "https://dl.dropboxusercontent.com/s/3kzrc04umi0ty40/RealmSwift.xcframework-10.24.2.zip?dl=0",
             checksum: "658a6bbf90f7628567b48824e438561602479a36b24bead4737bf117585773d6"
         )
     ]
